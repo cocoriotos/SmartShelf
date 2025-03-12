7dc055d9a593af8e96cd79316b36e23d91590f17
Julian GOnzalez <cocoriotos1@gmail.com>
2025-03-12 17:26:22 -0500

    admrole

--- lookmigrration/access_success_Tasks_final.php
+++ lookmigrration/access_success_Tasks_final.php
@@ -82,7 +82,6 @@ $global_username=$_POST['email'];
 $_SESSION['email']=$global_username;
 $local_username=$_SESSION['email'];
 $password=$_POST['password'];
-$admrole=0;
 
 	if($_POST)
  {
@@ -91,6 +90,7 @@ $admrole=0;
   $db_pass="C0mp13t3501ut10n5*";
   $db_name="u927778197_appsdb";
   $conn=mysqli_connect($db_host,$db_user,$db_pass,$db_name);
+  
 		if(mysqli_connect_errno()) 
 					{	
 					include("No_DB_Connectionfinal.php");
@@ -133,8 +133,16 @@ $admrole=0;
 				$query6="UPDATE videotips_suscription_payments SET suscriptiondaysleft = (365 - (DATEDIFF(CURDATE(), lastpaymentdate))) where username ='$local_username'"; 
 				$result6=mysqli_query($conn, $query6);
 
+				$query10="select adm_role from videotips_app_access_list where username ='$local_username'"; 
+				$result10=mysqli_query($conn, $query10);
+				
+				if ($result10 > 0){
+					header("refresh:0; url=appmgmt.php");
+					exit();
+				}
 
-				if ($suscriptiondaysleft > 16 && $suscriptionpayed == 0) {
+
+				if ($suscriptiondaysleft > 16 && $suscriptionpayed == 0 ) {
 					$_SESSION['suscriptiondue']=1;
 					header("refresh:0; url=suscriptionpayment.php");
 					exit();
@@ -143,16 +151,8 @@ $admrole=0;
 							{	
 								$query5="update videotips_app_access_list SET suscriptiondaysleft = DATEDIFF(CURDATE(), registrationdate), visits = visits+1 where username ='$local_username'"; 
 								$result5=mysqli_query($conn, $query5);
-								$query10="select adm_role from videotips_app_access_list where username ='$local_username'"; 
-								$result10=mysqli_query($conn, $query10);
-									if ($admrole > 0){
-											$admrole = 0;
-											header("refresh:0; url=appmgmt.php");
-											exit();
-									} else{
-											header("refresh:0; url=videolinkadminmodule.php");
-											exit();
-									}
+								header("refresh:0; url=videolinkadminmodule.php");
+								exit();
 							}
 							else 
 							{
