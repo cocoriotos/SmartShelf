c476bc82f51700074f3132bd4f8d7e9c6d7bd816
Julian GOnzalez <cocoriotos1@gmail.com>
2025-03-13 01:29:53 -0500

    contact us production

--- contact.php
+++ contact.php
@@ -1,9 +1,18 @@
 <?php
 session_start();
+$name = $_POST["name"];
+$email = $_POST["email"];
+$message = $_POST["message"];
 $captcha_input = $_POST["captcha"];
+$captchaok = 1;
+
+      
+
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (!isset($_POST["name"]) || !isset($_POST["email"]) || !isset($_POST["message"]) || !isset($_POST["captcha"])) {
-        echo "<script src='https://cdn.jsdelivr.net/npm/sweetalert2@11'></script>";
+
+  if (!isset($_POST["name"]) || !isset($_POST["email"]) || !isset($_POST["message"]) || !isset($_POST["captcha"])) {
+    echo "<script src='https://cdn.jsdelivr.net/npm/sweetalert2@11'></script>";
 		echo "<script>
 		document.addEventListener('DOMContentLoaded', function() {
 		Swal.fire({
@@ -19,10 +28,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
               });
           });
          </script>";
+         $captchaok = 0;
+         include ("index.php");
          exit();
-    }
-    
-    if (!isset($_SESSION["captcha"])) {
+  }
+  if (!isset($_SESSION["captcha"])) {
 		echo "<script src='https://cdn.jsdelivr.net/npm/sweetalert2@11'></script>";
       echo "<script>
       document.addEventListener('DOMContentLoaded', function() {
@@ -39,82 +49,60 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 });
             });
            </script>";
+           $captchaok = 0;
+           include ("index.php");
            exit();
     }
-
     // Comparar el CAPTCHA ingresado con el almacenado en sesión
     if (strtolower($captcha_input) !== strtolower($_SESSION["captcha"])) {
-		echo "<script src='https://cdn.jsdelivr.net/npm/sweetalert2@11'></script>";
-		echo "<script>
-		document.addEventListener('DOMContentLoaded', function() {
-		  Swal.fire({
-					title: 'Mensaje',
-					text: 'CAPTCHA incorrecto.',
-					icon: 'error',
-					confirmButtonText: 'Aceptar',
-					timer: 2000, // 2 segundos antes de redirigir
-					timerProgressBar: true, // Barra de progreso
-					willClose: () => {
-					  window.location.href = 'index.php'; // Redirección automática
-					}
-				  });
-			  });
-			 </script>";
-			 exit();
-    }
-
-    // Destruir el CAPTCHA después de la verificación para evitar reutilización
-    unset($_SESSION["captcha"]);
-
-
-    // Aquí puedes agregar la lógica para enviar el correo o guardar en una base de datos
-    // Ejemplo de envío de correo:
-    $to = "adm@solicionespro.com";
-    $subject = "Nuevo Mensaje de SmartShelf";
-    $body = "Nombre: $name\nCorreo: $email\nMensaje: $message";
-    $headers = "From: $email";
-
-    $mail = mail($to, $subject, $body, $headers);
-
-    if ($mail) {
-        // Alerta de éxito y redirección
-        echo "El Mensaje fué enviado correctamente";
-        echo "<script src='https://cdn.jsdelivr.net/npm/sweetalert2@11'></script>";
-        echo "<script>
-          document.addEventListener('DOMContentLoaded', function() {
-            Swal.fire({
-                title: 'Mensaje',
-                text: 'Gracias por contactarnos, estamos revisando tu mensaje y te responderemos pronto.',
-                icon: 'success',
-                confirmButtonText: 'Aceptar',
-                timer: 2000, // 2 segundos antes de redirigir
-                timerProgressBar: true, // Barra de progreso
-                willClose: () => {
-                  window.location.href = 'index.php'; // Redirección automática
-                }
-                });
-              });
-            </script>";
-            exit();
-    } else {
-        // Alerta de error y redirección
-        echo "<script>
-        document.addEventListener('DOMContentLoaded', function() {
-          Swal.fire({
-              title: 'Mensaje',
-              text: 'Hubo un error al enviar tu mensaje. Inténtalo de nuevo.',
-              icon: 'error',
-              confirmButtonText: 'Aceptar',
-              timer: 2000, // 2 segundos antes de redirigir
-              timerProgressBar: true, // Barra de progreso
-              willClose: () => {
-                window.location.href = 'index.php'; // Redirección automática
-              }
-              });
+      echo "<script src='https://cdn.jsdelivr.net/npm/sweetalert2@11'></script>";
+      echo "<script>
+      document.addEventListener('DOMContentLoaded', function() {
+        Swal.fire({
+            title: 'Mensaje',
+            text: 'CAPTCHA incorrecto.',
+            icon: 'error',
+            confirmButtonText: 'Aceptar',
+            timer: 2000, // 2 segundos antes de redirigir
+            timerProgressBar: true, // Barra de progreso
+            willClose: () => {
+              window.location.href = 'index.php'; // Redirección automática
+            }
             });
-          </script>";
-          exit();
-    }
+          });
+         </script>";
+         $captchaok = 0;
+         include ("index.php");
+         exit();
+      }
+      if ($captchaok = 1) {
+        $to1 = "cocoriotos@hotmail.com, adm@solicionespro.com";
+        $subject1 = "Nuevo Mensaje de SmartShelf";
+        $body1 = "Nombre: $name\nCorreo: $email\nMensaje: $message";
+        $headers1 = "From: adm@solicionespro.com";
+      
+        $mail3 = mail($to1, $subject1, $body1, $headers1);
+  
+          // Alerta de éxito y redirección
+          /*echo "El Mensaje fué enviado correctamente";*/
+          echo "<script src='https://cdn.jsdelivr.net/npm/sweetalert2@11'></script>";
+          echo "<script>
+            document.addEventListener('DOMContentLoaded', function() {
+              Swal.fire({
+                  title: 'Mensaje',
+                  text: 'Gracias por contactarnos, estamos revisando tu mensaje y te responderemos pronto.',
+                  icon: 'success',
+                  confirmButtonText: 'Aceptar',
+                  timer: 2000, // 2 segundos antes de redirigir
+                  timerProgressBar: true, // Barra de progreso
+                  willClose: () => {
+                    window.location.href = 'index.php'; // Redirección automática
+                  }
+                  });
+                });
+              </script>";
+              exit();
+      }
 }
-
 ?>
+
