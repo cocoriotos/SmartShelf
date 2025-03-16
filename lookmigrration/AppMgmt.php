aefc9bde0756e3fbe14862f5a51bbb81e80a1c55
Julian GOnzalez <cocoriotos1@gmail.com>
2025-03-16 17:05:26 -0500

    fixed

--- lookmigrration/AppMgmt.php
+++ lookmigrration/AppMgmt.php
@@ -22,53 +22,53 @@ $query4 = "update videotips_app_access_list SET suscriptiondaysleft = DATEDIFF(C
 $result4 = mysqli_query($conn, $query4);
 
 /*Consulta para contar los usuarios suscritos*/
-$query = "SELECT COUNT(suscriptionpayed) FROM videotips_app_access_list WHERE suscriptionpayed = 1 and suscriptionkind = 'De Pago'";
+$query = "SELECT COUNT(suscriptionpayed) as total_suscriptions FROM videotips_app_access_list WHERE suscriptionpayed = 1 and suscriptionkind = 'De Pago'";
 $result = mysqli_query($conn, $query);
 
-$query1 = "SELECT COUNT(active) FROM videotips_app_access_list WHERE active = 1";
+$query1 = "SELECT COUNT(active) as active_users FROM videotips_app_access_list WHERE active = 1";
 $result1 = mysqli_query($conn, $query1);
 
-$query2 = "SELECT COUNT(*) FROM videotips_app_access_list WHERE (active = 1 and suscriptionpayed = 1 and suscriptiondaysleft > 335 and suscriptionkind = 'De Pago')";
+$query2 = "SELECT COUNT(*) as suscritpionstodue FROM videotips_app_access_list WHERE (active = 1 and suscriptionpayed = 1 and suscriptiondaysleft > 335 and suscriptionkind = 'De Pago')";
 $result2 = mysqli_query($conn, $query2);
 
-/*$query3 = "SELECT COUNT(*) FROM videotips_accessrequests WHERE (processed = 'No'and granted = 'No')";
-$result3 = mysqli_query($conn, $query3);*/
+$query3 = "SELECT COUNT(*) as pendingaccess FROM videotips_accessrequests WHERE (processed = 'No'and granted = 'No')";
+$result3 = mysqli_query($conn, $query3);
 
-/*$query5 = "SELECT COUNT(*) FROM videotips_app_access_list  WHERE  365-suscriptiondaysleft between 0 and 30)";
+$query5 = "SELECT COUNT(*) as monthtoduesuscriptions FROM videotips_app_access_list  WHERE  365-suscriptiondaysleft between 0 and 30)";
 $result5 = mysqli_query($conn, $query5);
 
-$query6 = "SELECT COUNT(*) FROM videotips_app_access_list  WHERE  365-suscriptiondaysleft between 31 and 60)";
+$query6 = "SELECT COUNT(*) as twomothtoduesuscriptions FROM videotips_app_access_list  WHERE  365-suscriptiondaysleft between 31 and 60)";
 $result6 = mysqli_query($conn, $quer6);
 
-$query7 = "SELECT COUNT(*) FROM videotips_app_access_list  WHERE  365-suscriptiondaysleft between 61 and 90)";
-$result7 = mysqli_query($conn, $quer7);*/
+$query7 = "SELECT COUNT(*) as trimothtoduesuscriptions FROM videotips_app_access_list  WHERE  365-suscriptiondaysleft between 61 and 90)";
+$result7 = mysqli_query($conn, $quer7);
 
 
 if (($result) && ($result1)) {
     $row = mysqli_fetch_assoc($result);
     $total_suscriptions = $row['total_suscriptions'];
-    /*$row1 = mysqli_fetch_assoc($result1);
+    $row1 = mysqli_fetch_assoc($result1);
     $active_users = $row1['active_users'];
     $row2 = mysqli_fetch_assoc($result2);
     $suscriptionstodue = $row2['suscriptionstodue'];
     $row3 = mysqli_fetch_assoc($result3);
     $pendingaccess = $row3['pendingaccess'];
-    /*$row5 = mysqli_fetch_assoc($result5);
+    $row5 = mysqli_fetch_assoc($result5);
     $monthtoduesuscriptions = $row5['monthtoduesuscriptions'];
     $row6 = mysqli_fetch_assoc($result6);
     $twomothtoduesuscriptions = $row6['twomothtoduesuscriptions'];
     $row7 = mysqli_fetch_assoc($result7);
-    $trimothtoduesuscriptions = $row7['trimothtoduesuscriptions'];*/
+    $trimothtoduesuscriptions = $row7['trimothtoduesuscriptions'];
 
     
 } else {
     $total_suscriptores = 0; // En caso de error, mostrar 0
     $usuarios_activos = 0;
     $suscriptionstodue = 0;
-    /*$pendingaccess = 0;
+    $pendingaccess = 0;
     $monthtoduesuscriptions = 0;
     $twomothtoduesuscriptions = 0;
-    $trimothtoduesuscriptions = 0;*/
+    $trimothtoduesuscriptions = 0;
 }
 
 // Verificar si el usuario está autenticado (si $_SESSION['email'] está definido)
@@ -131,7 +131,7 @@ $local_username = $_SESSION['email']; // Obtener el email del usuario desde la s
                         </div>
                         <div class="grid-item-body">
                             <p class="p-title">Usuarios Activos:</p>
-                            <!--<center><p class="p-content"><?php /*echo $active_users;*/ ?></p></center>-->
+                            <center><p class="p-content"><?php /*echo $active_users;*/ ?></p></center>
                             <a href="#" class="btn-primary">Ver Detalles</a>
                         </div>
                     </div>
