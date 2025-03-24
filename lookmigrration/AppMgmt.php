3713d91983a910205261f4f72d2332268fa5f9ab
Julian GOnzalez <cocoriotos1@gmail.com>
2025-03-24 01:39:25 -0500

    trial

--- lookmigrration/AppMgmt.php
+++ lookmigrration/AppMgmt.php
@@ -18,7 +18,7 @@ include "db_connection1.php";
 $local_username = $_SESSION['email']; // Obtener el email del usuario desde la sesión
 
 /*Sync daysleft */
-$query4 = "update videotips_app_access_list SET suscriptiondaysleft = DATEDIFF(CURDATE(), lastsuscriptionpaymentdate)";
+$query4 = "update videotips_app_access_list SET suscriptiondaysleft = DATEDIFF(CURDATE(), lastsuscriptionpaymentdate), trialdaysleft = DATEDIFF(CURDATE(), registrationdate)";
 $result4 = mysqli_query($conn, $query4);
 
 /*Consulta para contar los usuarios suscritos*/
@@ -70,7 +70,7 @@ $result15 = mysqli_query($conn, $query15);
 $query16 = "SELECT COUNT(*) as twelvesuscriptions FROM videotips_app_access_list  WHERE  365-suscriptiondaysleft between 331 and 366";
 $result16 = mysqli_query($conn, $query16);
 
-$query17 = "UPDATE videotips_app_access_list SET active = 0, suscriptionactive = 0 WHERE (suscriptiondaysleft = DATEDIFF(CURDATE(), registrationdate)  > 16) and suscriptionkind = 'Trial'";
+$query17 = "UPDATE videotips_app_access_list SET active = 0, suscriptionactive = 0 WHERE trialdaysleft  > 16 and suscriptionkind = 'Trial'";
 $result17 = mysqli_query($conn, $query17);
 
 
