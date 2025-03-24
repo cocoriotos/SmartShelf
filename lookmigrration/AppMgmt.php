8410b0b1bf03463a61ce6b05da1658a2dd99fb6e
Julian GOnzalez <cocoriotos1@gmail.com>
2025-03-24 15:15:01 -0500

    resolve

--- lookmigrration/AppMgmt.php
+++ lookmigrration/AppMgmt.php
@@ -296,7 +296,7 @@ $local_username = $_SESSION['email']; // Obtener el email del usuario desde la s
                                     <tbody>
                                         <?php
                                         /* Consulta SQL */
-                                        /*$sql = "SELECT name, lastname, username, active, suscriptionactive, suscriptionkind FROM videotips_app_access_list where active = 0 and suscriptionactive = 0 and (suscriptionkind = 'Trial' or suscriptionkind = 'De Pago')";
+                                        $sql = "SELECT name, lastname, username, active, suscriptionactive, suscriptionkind FROM videotips_app_access_list where active = 0 and suscriptionactive = 0 and (suscriptionkind = 'Trial' or suscriptionkind = 'De Pago')";
                                         $result = $conn->query($sql);
                                         // Mostrar los resultados en la tabla
                                         if ($result->num_rows > 0) {
@@ -313,7 +313,7 @@ $local_username = $_SESSION['email']; // Obtener el email del usuario desde la s
                                                 }   
                                             } else {
                                         echo "<tr><td colspan='6'>No hay usuarios activos</td></tr>";
-                                        }*/
+                                        }
                                     ?>
                                     </tbody>
                             </table>
@@ -339,7 +339,7 @@ $local_username = $_SESSION['email']; // Obtener el email del usuario desde la s
                                 <tbody>
                                     <?php
                                         // Consulta SQL
-                                        /*$sql = "SELECT name, lastname, username, active, suscriptionactive, suscriptionkind, trialdaysleft FROM videotips_app_access_list where active = 1 and suscriptionactive = 0 and suscriptionkind = 'Trial' and trialdaysleft < 16)";
+                                        $sql = "SELECT name, lastname, username, active, suscriptionactive, suscriptionkind, trialdaysleft FROM videotips_app_access_list where active = 1 and suscriptionactive = 0 and suscriptionkind = 'Trial' and trialdaysleft < 16)";
                                         $result = $conn->query($sql);
                                         // Mostrar los resultados en la tabla
                                         if ($result->num_rows > 0) {
@@ -355,9 +355,9 @@ $local_username = $_SESSION['email']; // Obtener el email del usuario desde la s
                                                 echo "<td>" . $row['trialdaysleft'] . "</td>";
                                                 echo "</tr>";
                                              }
-                                        } else {*/
+                                        } else {
                                                  echo "<tr><td colspan='7'>No hay usuarios activos</td></tr>";
-                                        /*}*/
+                                        }
                                         ?>
                                 </tbody>
                             </table>
@@ -365,7 +365,7 @@ $local_username = $_SESSION['email']; // Obtener el email del usuario desde la s
                     </div>
                     </div>
             </div> 
-    </div>  
+      
               
 
     <script>
@@ -387,7 +387,7 @@ $local_username = $_SESSION['email']; // Obtener el email del usuario desde la s
         // Abrir la pestaña por defecto al cargar la página
         document.getElementById("defaultOpen").click();
     </script>
-
+</div>
 </body>
 
 </html>
