2702952c1da7306feb6ddf0921fc896b55b61635
Julian GOnzalez <cocoriotos1@gmail.com>
2025-03-31 16:24:56 -0500

    done

--- lookmigrration/access_success_Tasks_final.php
+++ lookmigrration/access_success_Tasks_final.php
@@ -158,7 +158,7 @@ $suscriptionkind = "None";
 				echo $suscriptionpayed;
 
 				if ($suscriptionkind = "Trial"){
-					$query11="UPDATE videotips_app_access_list SET suscriptiondaysleft = (DATEDIFF(CURDATE(), registrationdate)), trialdaysleft = (DATEDIFF(CURDATE(), registrationdate)) where username ='$local_username' and $suscriptionkind = 'Trial'"; 
+					$query11="UPDATE videotips_app_access_list SET suscriptiondaysleft = DATEDIFF(CURDATE(), registrationdate), trialdaysleft = DATEDIFF(CURDATE(), registrationdate) where username ='$local_username' and suscriptionkind = 'Trial'"; 
 					$result11=mysqli_query($conn, $query11);
 				}
 				
