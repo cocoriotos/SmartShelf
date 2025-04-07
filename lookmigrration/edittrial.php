dade42a1c61172d833680a380a921990809ae964
Julian GOnzalez <cocoriotos1@gmail.com>
2025-04-07 17:10:50 -0500

    aligning form text

--- lookmigrration/edittrial.php
+++ lookmigrration/edittrial.php
@@ -33,24 +33,31 @@ include "db_connection1.php";
 				<form action="updatesuscription.php" method="POST"> 
 					
 					<div class="form-group">
+                        <a>ID</a>
 						<input type="text" name="id" class="form-control" placeholder="id" autofocus value ="<?php echo $row['id'];?>" readonly></input><br>
 					</div>
 					<div class="form-group">
+                        <a>Usuario Activo</a>
 						<input type="text" name="active" class="form-control" placeholder="Usuario Activo" autofocus value ="<?php echo $row['active'];?>"></input><br>
 					</div>
 					<div class="form-group">
+                        <a>Suscripción Activa</a>
 						<input name="suscriptionactive" class="form-control" placeholder="Suscripción Activa"><?php echo $row['suscriptionactive']?></input>
 					</div>
 					<div class="form-group">
-						<input type="text" name="suscriptiondaysleft" class="form-control" placeholder="Suscription Days Used" value = "<?php echo $row['suscriptiondaysleft'];?>"  readonly></input><br>
+                        <a>Días usados de suscripción</a>
+						<input type="text" name="suscriptiondaysleft" class="form-control" placeholder="Dias de suscripción usados" value = "<?php echo $row['suscriptiondaysleft'];?>"  readonly></input><br>
 					</div>
 					<div class="form-group">
-						<input type="text" name="suscriptionpayed" class="form-control" placeholder="Suscription Payed" value = "<?php echo $row['suscriptionpayed'];?>"></input><br>
+                        <a>Suscripción Pagada</a>
+						<input type="text" name="suscriptionpayed" class="form-control" placeholder="Suscripción Pagada" value = "<?php echo $row['suscriptionpayed'];?>"></input><br>
 					</div>
 					<div class="form-group">
+                        <a>Última Fecha de Pago</a>
 						<input type="text" name="lastsuscriptionpaymentdate" class="form-control" placeholder="Última Fecha de Pago" value = "<?php echo $row['lastsuscriptionpaymentdate']?>"></input><br>
 					</div>
                     <div class="form-group">
+                        <a>Tipo de Suscripción</a>
 						<input type="text" name="suscriptionkind" class="form-control" placeholder="Tipo de Suscripción" value = "<?php echo $row['suscriptionkind']?>"></input><br>
 					</div>
 					<input type="submit" class="btn btn-success btn-block" name="update_suscription" value="Actualizar Suscripción"></input>
@@ -58,40 +65,7 @@ include "db_connection1.php";
 				</form>
 			</div>
 		</div>
-		
-		<div class="col-md-8">
-			<table class="table table-bordered">
-				<thead>
-				   <tr>
-	                  <th>ID</th>
-				      <th>Usuario Activo</th>
-					  <th>Suscripción Activa</th>
-					  <th>Dias usados de Suscripción</th>
-					  <th>Suscripcion Pagada</th>
-					  <th>Última fecha de pago Suscripción</th>
-					  <th>Tipo de Suscripción</th>
-				   </tr>
-			    </thead>
-				<tbody>
-					<?php 
-					$query1 = "select * from dailytasks_dailytask where completed = '0'  and id=$id";
-					$result_tasks1 = mysqli_query($conn,$query1);
-					while($row = mysqli_fetch_array($result_tasks1)) { ?>
-					  <tr>
-					    <td><?php echo $row['id'] ?></td>
-						<td><?php echo $row['active'] ?></td>
-						<td><?php echo $row['suscriptionactive'] ?></td>
-						<td><?php echo $row['suscriptiondaysleft'] ?></td>
-						<td><?php echo $row['suscriptionpayed'] ?></td>
-						<td><?php echo $row['lastsuscriptionpaymentdate'] ?></td>
-						<td><?php echo $row['suscriptionkind'] ?></td>
-						</td>
-					  </tr>
-					<?php }?>
-				<tbody>
-			</table>
-		</div>
-	</div>
+    </div>	
 </div>    
 </body>
 
