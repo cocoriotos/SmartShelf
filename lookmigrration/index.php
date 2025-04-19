9bc5974e5208bb261c216494538070a563cf20ff
Julian GOnzalez <cocoriotos1@gmail.com>
2025-04-19 13:35:43 -0500

    32

--- lookmigrration/index.php
+++ lookmigrration/index.php
@@ -23,7 +23,7 @@
     </header>
     <section class="hero">
         <div class="container">
-            <h1 style="font-size: 28px">Centraliza, Organiza y Accede a Todo tu Conocimiento en un Solo Lugar</h1>
+            <h1 style="font-size: 32px">Centraliza, Organiza y Accede a Todo tu Conocimiento en un Solo Lugar</h1>
             <p>¿Te has preguntado cuánto tiempo pierdes buscando enlaces, artículos, recursos o herramientas que guardaste pero no recuerdas dónde? Con <strong>SmartShelf</strong>, esa frustración se acabó.</p>
             <a href="videotrackerauth.php" class="btn">Ingresar</a>
             <a href="requestaccessfinal.php" class="btn">Registrarse</a>
