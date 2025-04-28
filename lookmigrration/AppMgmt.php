65a5101d9177520c1b83d497ce7789d8bb1572e1
Julian GOnzalez <cocoriotos1@gmail.com>
2025-04-27 23:08:57 -0500

    desc

--- lookmigrration/AppMgmt.php
+++ lookmigrration/AppMgmt.php
@@ -542,7 +542,7 @@ $local_username = $_SESSION['email']; // Obtener el email del usuario desde la s
                                                     <?php
                                                     // Consulta SQL
                                                     /*$sql = "SELECT name, lastname, username, suscriptionkind FROM videotips_app_access_list WHERE active = 1 order by suscriptionkind asc";*/
-                                                    $sql = "SELECT name, lastname, username, suscriptionkind, lastlogindate FROM videotips_app_access_list WHERE suscriptionkind <> 'De Pago' order by lastlogindate asc";
+                                                    $sql = "SELECT name, lastname, username, suscriptionkind, lastlogindate FROM videotips_app_access_list WHERE suscriptionkind <> 'De Pago' order by lastlogindate desc";
                                                     $result = $conn->query($sql);
                                                     // Mostrar los resultados en la tabla
                                                     if ($result->num_rows > 0) {
