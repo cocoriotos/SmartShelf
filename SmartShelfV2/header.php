aff6a774aba1b031be3d71663d4c5cf2717d1ced
raxwatchdog <raxwatchdog@gmail.com>
2026-05-23 21:32:58 -0500

    new design

--- SmartShelfV2/header.php
+++ SmartShelfV2/header.php
@@ -17,14 +17,13 @@ $name = $_SESSION['name'];
   <script src="copypaypal.js"></script>
 
   <nav class="navbar navbar-dark bg-dark d-flex justify-content-between align-items-center">
-      <div class="d-flex flex-wrap flex-grow-1">
-        <a id="headerfonts" href="suscriptionpayment.php" class="btn" style="background-color: #d0fff8; color: black; font-weight: bold;"><i class="fa fa-credit-card-alt" aria-hidden="true"></i> Suscribirse</a>
-        <a id="headerfonts" href="https://www.youtube.com/playlist?list=PLRQ5KF9igtB2GRlHLSP6Uwx1lzy387Wz5" class="btn" target="_blank" style="background-color: white; color: #c4302b; font-weight: bold;"><i class="fa fa-youtube-play" aria-hidden="true"></i> Tutoriales</a>
-        <a id="headerfonts" href="UCLToolManualDelUsuario2025.pdf" class="btn" target="_blank" style="background-color: white; color: black; font-weight: bold;"><i class="fa fa-file-pdf-o" aria-hidden="true"></i> Manual</a>
-      
-        <a id="headerfonts" href="videolinkadminmodule.php" class="btn" style="background-color: #FFF9CC; color: black; font-weight: bold;"><i class="fas fa-broom"></i> Limpiar Formulario</a>
-        <a id="headerfonts" href="addcategory.php" class="btn" style="background-color: #D6EEFF; color: black; font-weight: bold;"><i class="fas fa-layer-group"></i> Categorías</a>
-        <a id="headerfonts" href="closetaskscon.php" class="btn btn-danger" style="background-color: #9A97F5; color: black; font-weight: bold;"><i class="fas fa-sign-out-alt"></i> Salir</a>
+      <div class="header-action-group">
+        <a id="headerfonts" href="suscriptionpayment.php" class="header-action-btn subscribe"><i class="fas fa-gem"></i> Suscribirse</a>
+        <a id="headerfonts" href="https://www.youtube.com/playlist?list=PLRQ5KF9igtB2GRlHLSP6Uwx1lzy387Wz5" class="header-action-btn tutorials" target="_blank"><i class="fas fa-play-circle"></i> Tutoriales</a>
+        <a id="headerfonts" href="UCLToolManualDelUsuario2025.pdf" class="header-action-btn manual" target="_blank"><i class="fas fa-book-open"></i> Manual</a>
+        <a id="headerfonts" href="videolinkadminmodule.php" class="header-action-btn clear"><i class="fas fa-broom"></i> Limpiar Formulario</a>
+        <a id="headerfonts" href="addcategory.php" class="header-action-btn categories"><i class="fas fa-folder-tree"></i> Categorías</a>
+        <a id="headerfonts" href="closetaskscon.php" class="header-action-btn logout"><i class="fas fa-right-from-bracket"></i> Salir</a>
       </div>
   </nav>
 
--- SmartShelfV2/headereditcategory.php
+++ SmartShelfV2/headereditcategory.php
@@ -15,10 +15,10 @@ $name = $_SESSION['name'];
 
 <nav class="navbar navbar-dark bg-dark d-flex justify-content-between align-items-center">
      
-      <div class="d-flex flex-wrap flex-grow-1">
-          <a id="headerfonts" href="videolinkadminmodule.php" class="btn" style="background-color: #FFF9CC; color: black; font-weight: bold; margin-right: 10px;"><i class="fas fa-reply"></i> Adicionar Enlace</a>
-          <a id="headerfonts" href="addcategory.php" class="btn" style="background-color: rgb(214, 243, 255); color: black; font-weight: bold; margin-right: 10px;"><i class="fas fa-layer-group"></i> Categorías</a>  
-          <a id="headerfonts" href="closetaskscon.php" class="btn btn-danger" style="background-color: #9A97F5; color: black; font-weight: bold; margin-left: 10px; margin-right: 20px;"><i class="fas fa-sign-out-alt"></i> Salir</a>
+      <div class="header-action-group">
+          <a id="headerfonts" href="videolinkadminmodule.php" class="header-action-btn clear"><i class="fas fa-reply"></i> Adicionar Enlace</a>
+          <a id="headerfonts" href="addcategory.php" class="header-action-btn categories"><i class="fas fa-folder-tree"></i> Categorías</a>  
+          <a id="headerfonts" href="closetaskscon.php" class="header-action-btn logout"><i class="fas fa-right-from-bracket"></i> Salir</a>
       </div>
 </nav>
   <!-- BOOTSTRAP -->	
