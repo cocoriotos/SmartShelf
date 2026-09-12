9797ea3c6b3cefa5b20514fa8241b628c20e60de
raxwatchdog <raxwatchdog@gmail.com>
2026-09-11 20:33:30 -0500

    correct daystoenjoy for trial

--- SmartShelfV2/access_success_Tasks_final.php
+++ SmartShelfV2/access_success_Tasks_final.php
@@ -168,8 +168,8 @@ $active = 0;
 
 
 				//actualiza el estado de pago a vencido si el tiempo de suscripcion ha vencido
-				$query3="UPDATE videotips_app_access_list SET suscriptionkind = 'Vencida' where suscriptiondaysleft > 365 and suscriptionkind = 'De Pago' and username ='$local_username'"; 
-				$result3=mysqli_query($conn, $query3);
+				//$query3="UPDATE videotips_app_access_list SET suscriptionkind = 'Vencida' where suscriptiondaysleft > 365 and suscriptionkind = 'De Pago' and username ='$local_username'"; 
+				//$result3=mysqli_query($conn, $query3);
 
 				//deshabilitar la actualizacion masiva
 				$query4="SET SQL_SAFE_UPDATES = 1";
