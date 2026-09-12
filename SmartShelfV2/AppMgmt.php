f8f1a508883ee2c7fe56fda56e38ef0e3b5a58d1
raxwatchdog <raxwatchdog@gmail.com>
2026-09-11 21:33:12 -0500

    correct daystoenjoy for trial

--- SmartShelfV2/AppMgmt.php
+++ SmartShelfV2/AppMgmt.php
@@ -75,7 +75,7 @@ $result2=mysqli_query($conn, $query2);
 
 
 //actualiza el estado de pago a suspendido si el tiempo de suscripcion ha vencido
-$query9="UPDATE videotips_app_access_list SET suscriptionkind = 'Vencida' where daystoenjoy > 365 and suscriptionpayed = 0 and (suscriptionkind = 'De Pago' or suscriptionkind = 'Vencida' or suscriptionkind = 'Suspendida' or suscriptionkind = 'vencida')"; 
+$query9="UPDATE videotips_app_access_list SET suscriptionkind = 'Vencida' where daystoenjoy > 365 and suscriptionpayed = 0 and (suscriptionkind = 'De Pago' or suscriptionkind = 'Vencida' or suscriptionkind = 'Suspendida' or suscriptionkind = 'Trial')"; 
 $result9=mysqli_query($conn, $query9);
 
 /*Consulta para contar los usuarios suscritos*/
