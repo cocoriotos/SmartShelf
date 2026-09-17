8dcd3c46578fda769727db213c5c26766f2e3ffd
raxwatchdog <raxwatchdog@gmail.com>
2026-09-17 01:59:36 -0500

    alineamiento de botones y correo

--- SmartShelfV2/requestaccessfinal.php
+++ SmartShelfV2/requestaccessfinal.php
@@ -6,7 +6,7 @@
     <link rel="icon" href="SSCircleBackgroundWhite.ico" type="image/x-icon">
     <script src="head.js" defer></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-    <link rel="stylesheet" href="style_sheet_auth.css">
+    <link rel="stylesheet" href="style_sheet_auth.css?v=20260917">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
     <!-- SweetAlert2 CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
