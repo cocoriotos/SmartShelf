cba87ec74ce7410cdb4dc9ac05bd5c9bd94297d0
raxwatchdog <raxwatchdog@gmail.com>
2026-09-19 11:16:38 -0500

    Logo GORA

--- SmartShelfV2/recoverpassword.php
+++ SmartShelfV2/recoverpassword.php
@@ -6,8 +6,8 @@ include "sessions.php";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Recuperar Contraseña - SmartShelf</title>
-    <link rel="icon" href="SSCircleBackgroundWhite.ico" type="image/x-icon">
+    <title>Recuperar Contraseña - G O R A</title>
+    <link rel="icon" href="GORA.ico" type="image/x-icon">
     <script src="head.js" defer></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link rel="stylesheet" href="style_sheet_auth.css">
@@ -25,7 +25,7 @@ include "sessions.php";
 
         <!-- Encabezado del formulario -->
         <div class="login-header">
-            <img src="SmartShelfUsefulContentLibraryDarrkLightGreen.ico" alt="SmartShelf Logo" class="logo">
+            <img src="GORAarmony.png" alt="GORA Logo" class="logo">
             <h1 data-i18n="rec_title">Recuperación de Contraseña</h1>
         </div>
 
