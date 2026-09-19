24a133978fe00edbd723093499e88a665aa75c05
raxwatchdog <raxwatchdog@gmail.com>
2026-09-19 03:27:02 -0500

    Index GORA

--- SmartShelfV2/index.php
+++ SmartShelfV2/index.php
@@ -13,7 +13,7 @@
 <!-- NAV -->
 <nav>
   <a href="#" class="nav-logo">
-    <img src="GORAarminy.png" alt="G O R A"/>
+    <img src="GORAarmony.png" alt="G O R A"/>
   </a>
   <ul class="nav-links">
     <li><a href="#features" data-i18n="nav_features">Características</a></li>
