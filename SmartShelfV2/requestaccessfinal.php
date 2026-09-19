be5d5b805a31efcec36fad5b2364ddd1bba90198
raxwatchdog <raxwatchdog@gmail.com>
2026-09-19 10:46:28 -0500

    Logo GORA

--- SmartShelfV2/requestaccessfinal.php
+++ SmartShelfV2/requestaccessfinal.php
@@ -24,7 +24,7 @@
 
         <!-- Encabezado del formulario -->
         <div class="login-header">
-            <img src="GORAarmony.ico" alt="GORA Logo" class="logo">
+            <img src="GORAarmony.png" alt="GORA Logo" class="logo">
             <h1 data-i18n="req_title">Solicitud de Acceso</h1>
         </div>
 
