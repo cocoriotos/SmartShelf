9902bf700b60cf639ef6f3d356d57422ad0afcb3
raxwatchdog <raxwatchdog@gmail.com>
2026-09-21 22:48:35 -0500

    suscription whatsapp

--- SmartShelfV2/suscriptionpayment.php
+++ SmartShelfV2/suscriptionpayment.php
@@ -296,12 +296,13 @@ $suscriptiondue = $_SESSION['suscriptiondue'];
 
                 <!--<center><input type="submit" class="btn btn-success btn-block" name="save_link" value="Agregar Constancia de Pago"></input></center>-->
         </div>
-  </div>
-</div>
-<a href="https://wa.me/573117592209" target="_blank" class="whatsapp-fab">
-        <img src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg" alt="WhatsApp">
+        <a href="https://wa.me/573117592209" target="_blank" class="whatsapp-fab">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg" alt="WhatsApp">
         <span id="support-label">Soporte</span>
     </a>
+  </div>
+</div>
+
 </body>
 
 <script>
