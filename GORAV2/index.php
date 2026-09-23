59aa7e0efe1e2824ea92833b2eb87f2440037b51
cocoriotos1 <cocoriotos1@gmail.com>
2026-09-23 00:49:36 -0500

    index to GORA

--- GORAV2/index.php
+++ GORAV2/index.php
@@ -3,7 +3,7 @@
 <head>
 <meta charset="UTF-8"/>
 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-<title>GORA — Tu Biblioteca Digital Inteligente</title>
+<title>GORA GORA— Tu Biblioteca Digital Inteligente</title>
 <link rel="icon" href="GORA.ico"/>
 <link rel="stylesheet" href="smartshelf.css"/>
 </head>
