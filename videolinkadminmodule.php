eedc3d2dcd02e1d3cd27f8bd5395f6a5149d8b20
cocoriotos1 <cocoriotos1@gmail.com>
2026-09-25 15:37:47 -0500

    Icon

--- videolinkadminmodule.php
+++ videolinkadminmodule.php
@@ -24,7 +24,7 @@ include "header.php";
 <head>
     <script src="head.js" defer></script>
     <script src="Linktoclipboard.js" defer></script>
-    <link rel="icon" href="SSCircleBackgroundWhite.ico" type="image/x-icon">
+    <link rel="icon" href="GORA.ico" type="image/x-icon">
     <link rel="stylesheet" href="style_sheet_ops.css" />
     <script src="Popper/popper.min.js"></script>
     <script src="plugins/sweetalert/sweetalert.min.js"></script>
